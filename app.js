f822e00ef6ad17dfeba1e5322fb7efc12a388881
slsuyel <slsuyel@gmail.com>
2023-02-14 15:52:18 +0600

    clear btn add

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ function change(ID, value){
     document.getElementById(ID).style.textAlign = value;
 }
 
+function clearStyle(){
+ const text =document.getElementById('text-valu').value;
+ document.getElementById('text-valu').style.fontStyle = "normal";
+ change('text-valu',"left");
+ document.getElementById('text-valu').style.fontWeight = "400";
+ document.getElementById('text-valu').style.color = "black";
+
+}
+
 document.getElementById('btn-right').addEventListener('click',function(){
 change('text-valu',"right")
 })
@@ -24,4 +33,8 @@ document.getElementById('text-valu').style.fontStyle = "italic";
 document.getElementById('color-code').addEventListener('change',function(e){
     const text = document.getElementById('text-valu');
     text.style.color = e.target.value; 
+})
+document.getElementById('clear-btn').addEventListener('click',function(){
+    document.getElementById('text-valu').value = ''
+    clearStyle()
 })
